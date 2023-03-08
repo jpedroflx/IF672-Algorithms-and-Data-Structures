38f2a04e4cf54950d152f6a68a743c7214e71a60
João Pedro Felix <65830961+jpedroflx@users.noreply.github.com>
2023-03-08 02:05:18 -0300

    Update Sequential Search.cpp
    O algoritmo de busca sequencial é implementado na função sequentialSearch, que recebe como argumentos um vetor e um valor alvo a ser procurado. A função percorre o vetor da esquerda para a direita, comparando cada elemento com o valor alvo. Se o elemento for igual ao valor alvo, a função retorna o índice desse elemento. Caso contrário, a função retorna -1 para indicar que o valor alvo não foi encontrado no vetor. No exemplo acima, um vetor de inteiros é criado e um valor alvo é definido como 8. Em seguida, a função sequentialSearch é chamada com esses argumentos e o resultado é impresso na tela.

--- Implementation/Sequential Search.cpp
+++ Implementation/Sequential Search.cpp
@@ -3,24 +3,26 @@
 
 using namespace std;
 
-int sequentialSearch(vector<int>& arr, int target) {
-    for (int i = 0; i < arr.size(); i++) {
-        if (arr[i] == target) {
-            return i;
+int linearSearch(vector<int>& arr, int target) {
+    for (int i = 0; i < arr.size(); i++) {   // percorre a lista linearmente
+        if (arr[i] == target) {             // se encontrar o elemento desejado
+            return i;                       // retorna sua posição na lista
         }
     }
-    return -1;
+
+    return -1;  // caso não encontre o elemento, retorna -1
 }
 
 int main() {
-    vector<int> arr = { 9, 2, 7, 5, 1, 8, 3, 6, 4 };
-    int target = 8;
-    int index = sequentialSearch(arr, target);
-    if (index != -1) {
-        cout << "O elemento " << target << " foi encontrado na posição " << index << endl;
+    vector<int> arr = {1, 3, 5, 7, 9, 11, 13};
+    int target = 7;
+
+    int index = linearSearch(arr, target);
+    if (index == -1) {
+        cout << "Elemento nao encontrado." << endl;
+    } else {
+        cout << "Elemento encontrado na posicao " << index << "." << endl;
     }
-    else {
-        cout << "O elemento " << target << " não foi encontrado no vetor" << endl;
-    }
+
     return 0;
 }
