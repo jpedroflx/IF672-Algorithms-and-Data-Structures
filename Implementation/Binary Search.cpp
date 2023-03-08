b1108b497148903067258a2c238df1f6e06205f5
João Pedro Felix <65830961+jpedroflx@users.noreply.github.com>
2023-03-08 02:02:11 -0300

    Create Binary Search.cpp
    O algoritmo de busca binária é implementado na função binarySearch, que recebe como argumentos um vetor e um valor alvo a ser procurado. A função utiliza dois índices, left e right, para indicar a faixa de valores do vetor em que o valor alvo pode estar. Em cada iteração do loop while, a função calcula o índice do elemento central da faixa e o compara com o valor alvo. Se o elemento central for igual ao valor alvo, a função retorna o índice desse elemento. Caso contrário, a função atualiza os índices left e right de acordo com o resultado da comparação, reduzindo assim a faixa de valores em que o valor alvo pode estar. Se a faixa de valores ficar vazia, a função retorna -1 para indicar que o valor alvo não foi encontrado no vetor. No exemplo acima, um vetor ordenado de inteiros é criado e um valor alvo é definido como 6. Em seguida, a função binarySearch é chamada com esses argumentos e o resultado é impresso na tela.

---
+++ Implementation/Binary Search.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int binarySearch(vector<int>& arr, int target) {
+    int left = 0;                   // índice do primeiro elemento da sublista
+    int right = arr.size() - 1;     // índice do último elemento da sublista
+
+    while (left <= right) {         // enquanto a sublista não for vazia
+        int mid = (left + right) / 2;   // índice do elemento do meio da sublista
+        if (arr[mid] == target) {       // se encontrou o elemento
+            return mid;
+        } else if (arr[mid] < target) { // se o elemento do meio é menor que o alvo
+            left = mid + 1;             // descarta a metade esquerda da sublista
+        } else {                        // se o elemento do meio é maior que o alvo
+            right = mid - 1;            // descarta a metade direita da sublista
+        }
+    }
+
+    return -1;  // caso não encontre o elemento, retorna -1
+}
+
+int main() {
+    vector<int> arr = {1, 3, 5, 7, 9, 11, 13};
+    int target = 7;
+
+    int index = binarySearch(arr, target);
+    if (index == -1) {
+        cout << "Elemento nao encontrado." << endl;
+    } else {
+        cout << "Elemento encontrado na posicao " << index << "." << endl;
+    }
+
+    return 0;
+}
