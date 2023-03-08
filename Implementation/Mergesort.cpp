90a76e6253993b5aa8f49d770b7fb3c396d23898
João Pedro Felix <65830961+jpedroflx@users.noreply.github.com>
2023-03-08 02:07:17 -0300

    Update Mergesort.cpp

--- Implementation/Mergesort.cpp
+++ Implementation/Mergesort.cpp
@@ -3,52 +3,73 @@
 
 using namespace std;
 
-void merge(vector<int>& arr, int left, int mid, int right) {
-    vector<int> temp(right - left + 1);
-    int i = left, j = mid + 1, k = 0;
-    while (i <= mid && j <= right) {
-        if (arr[i] <= arr[j]) {
-            temp[k] = arr[i];
-            k++;
+// função auxiliar que combina dois subarrays ordenados
+void merge(vector<int>& arr, int left, int middle, int right) {
+    int n1 = middle - left + 1;     // tamanho do primeiro subarray
+    int n2 = right - middle;        // tamanho do segundo subarray
+
+    // cria subarrays temporários
+    vector<int> leftArr(n1);
+    vector<int> rightArr(n2);
+
+    // copia os elementos para os subarrays temporários
+    for (int i = 0; i < n1; i++) {
+        leftArr[i] = arr[left + i];
+    }
+    for (int j = 0; j < n2; j++) {
+        rightArr[j] = arr[middle + 1 + j];
+    }
+
+    // combina os subarrays temporários de volta no array original
+    int i = 0;      // índice do primeiro subarray
+    int j = 0;      // índice do segundo subarray
+    int k = left;   // índice do array combinado
+    while (i < n1 && j < n2) {
+        if (leftArr[i] <= rightArr[j]) {
+            arr[k] = leftArr[i];
             i++;
-        }
-        else {
-            temp[k] = arr[j];
-            k++;
+        } else {
+            arr[k] = rightArr[j];
             j++;
         }
-    }
-    while (i <= mid) {
-        temp[k] = arr[i];
         k++;
+    }
+
+    // copia os elementos restantes do subarray esquerdo, se houver
+    while (i < n1) {
+        arr[k] = leftArr[i];
         i++;
-    }
-    while (j <= right) {
-        temp[k] = arr[j];
         k++;
-        j++;
     }
-    for (i = left; i <= right; i++) {
-        arr[i] = temp[i - left];
+
+    // copia os elementos restantes do subarray direito, se houver
+    while (j < n2) {
+        arr[k] = rightArr[j];
+        j++;
+        k++;
     }
 }
 
+// função principal do Merge Sort
 void mergeSort(vector<int>& arr, int left, int right) {
-    if (left >= right) {
-        return;
+    if (left < right) {
+        int middle = left + (right - left) / 2;  // encontra o meio do array
+        mergeSort(arr, left, middle);            // chama recursivamente para o subarray esquerdo
+        mergeSort(arr, middle + 1, right);       // chama recursivamente para o subarray direito
+        merge(arr, left, middle, right);         // combina os subarrays ordenados
     }
-    int mid = (left + right) / 2;
-    mergeSort(arr, left, mid);
-    mergeSort(arr, mid + 1, right);
-    merge(arr, left, mid, right);
 }
 
 int main() {
-    vector<int> arr = { 9, 2, 7, 5, 1, 8, 3, 6, 4 };
-    mergeSort(arr, 0, arr.size() - 1);
-    for (int i = 0; i < arr.size(); i++) {
+    vector<int> arr = {5, 2, 4, 6, 1, 3};
+    int n = arr.size();
+
+    mergeSort(arr, 0, n - 1);
+
+    for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
     cout << endl;
+
     return 0;
 }
